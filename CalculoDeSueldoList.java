f5fea6508b65a5de2885b2f1ec0931748ddc4d04
Apo1231 <20243ds166@utez.edu.mx>
2025-09-11 08:24:47 -0600

    Creacion de clase CalculoDeSueldoList

---
+++ CalculoDeSueldoList.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class CalculoDeSueldoList {
+    public static void main(String[] args) {
+    List <Worker> worker = new ArrayList<>();
+
+
+
+    }
+}
